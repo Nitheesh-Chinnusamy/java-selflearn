48e268ecbb4a9efc5439e20516263ae3c89f2c7a
Nitheesh-Chinnusamy <nitheeshchinnusamy21@gmail.com>
2025-03-27 22:53:51 +0530

    updated the pattern code

--- AlphabetPattern.java
+++ AlphabetPattern.java
@@ -2,20 +2,17 @@
 public class AlphabetPattern {
 
     public static void main(String[] args) {
-        printPattern();
-    }
-
-    public static void printPattern() {
-        char ch = 'a'; // Start from 'a'
+        char ch = 'a'; // I have created variable without Ascii value
 
         for (int i = 0; i < 3; i++) { // 3 rows
             for (int j = 0; j < 3; j++) { // 3 columns
-                if (i == 2 && j == 1) { // Special case to maintain pattern alignment
-                    System.out.print("   ");
-                    continue;
+                //Skipping alphabets
+                while (ch == 'c' || ch == 'f' || ch == 'i' || ch == 'l'
+                        || ch == 'o' || ch == 'r' || ch == 'u' || ch == 'x') {
+                    ch++;
                 }
                 System.out.print(ch + "" + (char) (ch + 1) + "   ");
-                ch += 2;
+                ch += 2; // Move to the next letter pair
             }
             System.out.println();
         }
